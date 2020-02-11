fe43357510cfb47f1087edda00cfc064116b3e0a
Sarah Schwartz <seschwartz8@gmail.com>
2020-02-11 14:16:56 -0800

    abstract out number array specific sorting and swapping into NumbersCollection class

--- build/index.js
+++ build/index.js
@@ -1,23 +1,6 @@
 "use strict";
-var Sorter = /** @class */ (function () {
-    function Sorter(collection) {
-        this.collection = collection;
-    }
-    Sorter.prototype.sort = function () {
-        var length = this.collection.length;
-        // Using bubble sort for simplicity (disregard inefficiency)
-        for (var i = 0; i < length; i++) {
-            for (var j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    var leftHand = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = leftHand;
-                }
-            }
-        }
-    };
-    return Sorter;
-}());
-var sorter = new Sorter([3, 4, 15, 10]);
+Object.defineProperty(exports, "__esModule", { value: true });
+var Sorter_1 = require("./Sorter");
+var sorter = new Sorter_1.Sorter([3, 4, 15, 10]);
 sorter.sort();
 console.log(sorter.collection);
